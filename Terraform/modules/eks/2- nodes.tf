9c50f9fe630c079b352b727966788de7734a5c20
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2025-08-28 18:32:26 +0300

    add vpc and structure refactoring

--- Terraform/modules/eks/2- nodes.tf
+++ Terraform/modules/eks/2- nodes.tf
@@ -1,38 +1,9 @@
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-  }
-}
-
-resource "aws_iam_role" "eks_cluster" {
-  name = "${var.cluster_name}-eks-cluster-role"
-
-  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
-}
-
-data "aws_iam_policy_document" "eks_cluster_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster.name
-}
-
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-nodes"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  subnet_ids      = var.subnet_ids
+
   scaling_config {
     desired_size = var.node_desired_capacity
     max_size     = 3
--- Terraform/terraform.tfvars
+++ Terraform/terraform.tfvars
@@ -1,5 +1,7 @@
-region               = "us-west-2"
-vpc_cidr             = "10.0.0.0/16"
-public_key_path      = "id_rsa.pub"
-cluster_name         = "voting-app"
+region                = "us-west-2"
+vpc_cidr              = "10.0.0.0/16"
+cluster_name          = "voting-app"
 node_desired_capacity = 2
+public_subnets_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnets_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+availability_zones    = ["us-west-1a", "us-west-1b"]
