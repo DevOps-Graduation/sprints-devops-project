05af2ef1ebd533749dda77cd866857ec31cedb7e
sarah <ibrahimsarah839@gmail.com>
2025-08-31 00:15:28 +0300

    data resource in helm

--- Terraform/modules/monitoring/main.tf
+++ Terraform/modules/monitoring/main.tf
@@ -6,14 +6,14 @@ data "aws_eks_cluster_auth" "this" {
   name = data.aws_eks_cluster.this.name
 }
 
-
 provider "helm" {
   kubernetes = {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
+
 
 resource "helm_release" "prometheus" {
   name       = "prometheus"
