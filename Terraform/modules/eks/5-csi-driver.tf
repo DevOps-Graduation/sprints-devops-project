66b5a605b223bbe49865a74f53f0b1d515a7515b
mmtrabya <midotarabay66@gmail.com>
2025-08-30 16:08:32 +0300

    Added CSI Driver for EBS to EKS

---
+++ Terraform/modules/eks/5-csi-driver.tf
@@ -0,0 +1,47 @@
+# Fetch cluster details (if not already available in your module)
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+# OIDC provider for IRSA
+resource "aws_iam_openid_connect_provider" "eks" {
+  url            = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list = ["sts.amazonaws.com"]
+}
+
+# IAM Role for EBS CSI Driver (using IRSA)
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach AWS managed policy for EBS CSI
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_driver.name
+}
+
+# Install the EBS CSI driver as an addon
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = data.aws_eks_cluster.this.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.29.1-eksbuild.1"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+}
